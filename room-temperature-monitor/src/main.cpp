693ed5b74f78b4914de13eed8970dce622809b82
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2023-05-17 20:22:46 +0200

    feat(room-temperature-monitor): setup wifi connection

--- room-temperature-monitor/src/main.cpp
+++ room-temperature-monitor/src/main.cpp
@@ -1,22 +1,46 @@
 #include <Arduino.h>
-
 #include <DS18B20.h> // https://github.com/matmunk/DS18B20
 DS18B20 ds(5);       // pin GPIO5
+#include <WiFi.h>
+#include "wifi_credentials.h"
 
 uint8_t address[8];
 char addressString[20];
 
-void setup(void)
+void connectToWifi()
 {
-  Serial.begin(115200);
-  delay(1000);
+  WiFi.mode(WIFI_STA); // Optional
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("\nConnecting");
 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void initDS18B20()
+{
   ds.selectNext();
   ds.getAddress(address);
   for (int j = 0; j < sizeof(address); j++)
   {
     sprintf(&addressString[j * 2], "%02X", address[j]);
   }
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  initDS18B20();
+  connectToWifi();
 
   Serial.println();
   Serial.println("Init OK");
